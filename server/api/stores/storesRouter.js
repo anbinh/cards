d461411a92c02da7a0ebdc86c62d26714e55130a
anbinh <tinhoc@outlook.com>
2015-07-07 07:58:23 -0400

    update getting stores by gogo_discount

--- server/api/stores/storesRouter.js
+++ server/api/stores/storesRouter.js
@@ -3,461 +3,164 @@
 var express = require('express');
 var router = express.Router();
 
+var StoresCat = {
+    "target": "B",
+    "walmart": "B",
+    "marshalls": "B",
+    "tj-maxx": "B",
+    "cvs": "B",
+    "sears": "B",
+    "babies-r-us": "B",
+    "office-depot": "B",
+    "pet-co": "B",
+    "walgreens": "B",
+    "staples": "B",
+    "pet-smart": "B",
+    "advance-auto-parts": "B",
+    "brookstone": "B",
+    "kmart": "B",
+    "rite-aid": "B",
+    "auto-zone": "B",
+    "office-max": "B",
+    "family-dollar": "B",
+    "macys": "C",
+    "bloomingdales": "C",
+    "old-navy": "C",
+    "gap": "C",
+    "victorias-secret": "C",
+    "nordstrom": "C",
+    "american-eagle": "C",
+    "jcpenney": "C",
+    "dsw": "C",
+    "forever-21": "C",
+    "ross": "C",
+    "nike": "C",
+    "express": "C",
+    "h-and-m": "C",
+    "lane-bryant": "C",
+    "pac-sun": "C",
+    "lord-and-taylor": "C",
+    "neiman-marcus": "C",
+    "lands-end": "C",
+    "famous-footwear": "C",
+    "abercrombie-and-fitch": "C",
+    "i-tunes": "E",
+    "amazon": "E",
+    "best-buy": "E",
+    "game-stop": "E",
+    "toys-r-us": "E",
+    "amc-theatres": "E",
+    "fandango": "E",
+    "regal-cinemas": "E",
+    "regal-entertainment": "E",
+    "hyatt-hotels": "E",
+    "southwest-airlines": "E",
+    "apple": "E",
+    "dicks-sporting-goods": "E",
+    "sports-authority": "E",
+    "bass-pro-shops": "E",
+    "starbucks": "F",
+    "applebees": "F",
+    "outback-steakhouse": "F",
+    "dunkin-donuts": "F",
+    "tgi-fridays": "F",
+    "mcdonalds": "F",
+    "whole-foods": "F",
+    "chilis-restaurants": "F",
+    "darden-restaurants": "F",
+    "red-lobster": "F",
+    "flemings": "F",
+    "cold-stone-creamery": "F",
+    "ihop": "F",
+    "subway": "F",
+    "ruths-chris-steak-house": "F",
+    "panera-bread": "F",
+    "chipotle": "F",
+    "macaroni-grill": "F",
+    "coffee-bean-and-tea-leaf": "F",
+    "dominos-pizza": "F",
+    "smoothie-king": "F",
+    "home-depot": "H",
+    "lowes": "H",
+    "sephora": "H",
+    "kohls": "H",
+    "bath-and-body-works": "H",
+    "home-goods": "H",
+    "bed-bath-and-beyond": "H",
+    "pottery-barn": "H",
+    "shell": "H",
+    "crate-and-barrel": "H",
+    "michaels": "H",
+    "bp-amoco": "H",
+    "williams-sonoma": "H",
+    "buybuy-baby": "H"
+};
+
 
 // Get all store
 router.get('/', function(req, res, next) {
-    var data = [{
-        "name": "target",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "walmart",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "marshalls",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "tj-maxx",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "cvs",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "sears",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "babies-r-us",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "office-depot",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "pet-co",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "walgreens",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "staples",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "pet-smart",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "advance-auto-parts",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "brookstone",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "kmart",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "rite-aid",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "auto-zone",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "office-max",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "family-dollar",
-        "category": "B",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "macys",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "bloomingdales",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "old-navy",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "gap",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "victorias-secret",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "nordstrom",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "american-eagle",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "jcpenney",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "dsw",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "forever-21",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "ross",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "nike",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "express",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "h-and-m",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "lane-bryant",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "pac-sun",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "lord-and-taylor",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "neiman-marcus",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "lands-end",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "famous-footwear",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "abercrombie-and-fitch",
-        "category": "C",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "i-tunes",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "amazon",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "best-buy",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "game-stop",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "toys-r-us",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "amc-theatres",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "fandango",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "regal-cinemas",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "regal-entertainment",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "hyatt-hotels",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "southwest-airlines",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "apple",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "dicks-sporting-goods",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "sports-authority",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "bass-pro-shops",
-        "category": "E",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "starbucks",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "applebees",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "outback-steakhouse",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "dunkin-donuts",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "tgi-fridays",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "mcdonalds",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "whole-foods",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "chilis-restaurants",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "darden-restaurants",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "red-lobster",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "flemings",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "cold-stone-creamery",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "ihop",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "subway",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "ruths-chris-steak-house",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "panera-bread",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "chipotle",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "macaroni-grill",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "coffee-bean-and-tea-leaf",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "dominos-pizza",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "smoothie-king",
-        "category": "F",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "home-depot",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "lowes",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "sephora",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "kohls",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "bath-and-body-works",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "home-goods",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "bed-bath-and-beyond",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "pottery-barn",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "shell",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "crate-and-barrel",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "michaels",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "bp-amoco",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "williams-sonoma",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }, {
-        "name": "buybuy-baby",
-        "category": "H",
-        "discount": Math.random() * 80 | 0,
-        "price": Math.random() * 300 | 0
-    }];
-    res.json(data);
+    req.getConnection(function(err, connection) {
+        if (err) return next(err);
+        connection.query('select max(id) as id from cards group by store_id', [], function(err, rows) {
+            if (err) return next(err);
+
+
+            var ids = [];
+            for (var i = 0; i < rows.length; i++) {
+                ids.push(rows[i].id);
+            };
+
+            ids = ids.join(",");
+
+            // console.log(ids);
+
+            connection.query('select stores.id, stores.name as name, cards.discount, stats.gcg_buy,stats.gogo_discount_extra, stats.fixed_discount from cards LEFT JOIN stores ON stores.id = cards.store_id   LEFT JOIN stats ON stats.id=cards.store_id where cards.id in ( ' + ids + ' ) order by store_id DESC', [], function(err, rows) {
+                if (err) return next(err);
+
+                var stores = [];
+                for (var i = 0; i < rows.length; i++) {
+                    var store = rows[i];
+                    if (store.fixed_discount > 0) {
+                        store.discount = store.fixed_discount;
+                    }
+
+                    store.name = store.name.trim();
+
+
+                    if (StoresCat[store.name] != undefined) {
+                        store.category = StoresCat[store.name];
+                    }
+
+
+
+
+                    store.gogo_discount = (parseFloat(store.discount)) + parseFloat(store.gogo_discount_extra);
+
+
+                    if (parseFloat(store.gcg_buy) != 0) {
+                        store.gogo_buy = parseFloat(store.gcg_buy) + 1;
+                        var t = Math.floor((100 - store.gogo_discount - parseFloat(store.gogo_buy)) * 100) / 100;
+                        store.spread = Math.floor(t / (100 - store.gogo_discount) * 100000) / 1000;
+
+                        // console.log("t init",store.gogo_discount,store.gogo_buy ,t);
+                    } else {
+                        store.gogo_buy = 1;
+                        store.spread = "";
+                    }
+
+
+                    stores.push(store);
+
+
+                };
+
+                res.json(stores);
+            });
+
+
+        });
+
+    });
 });
 
 
